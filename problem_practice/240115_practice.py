2a767c63320cd0c9b4067c525c0f12ffbd703329
강두홍 <dhkang960912@gmail.com>
2024-01-15 14:50:30 +0900

    TIL과 240115 문제풀이

---
+++ problem_practice/240115_practice.py
@@ -0,0 +1,65 @@
+# 문자열 심화
+# password = "In the bustling city, where life is a constant race against time, uoy often find yourself wondering if there's a shortcut to success. The vibrant lights of the cityscape illuminate the night, casting shadows on the short-lived dreams of those who seek fortune. As you navigate through the crowded streets, you realize the deen for guidance, like a compass pointing python. You need direction in this chaotic journey called life."
+# first_char = password[28 : 36]
+# second_word = password[113 : 118]
+# third_word = password[69:65:-1]
+# fourth_word = password[326:321:-1]
+# fifth_word = password[365:371]
+
+# print(f'{first_char}{second_word}{third_word}{fourth_word} "{fifth_word}".')
+
+
+# 원의 넓이와 둘레변수
+# 원주율
+# 원주율 = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+# 반지름
+# 반지름 = 15
+# 원의_둘레 = 반지름 * 2 * 원주율
+# 원의_넓이 = 반지름 * 반지름 * 원주율
+
+# print('원주율 : ', 원주율) 
+# print('반지름 : ', 반지름)
+# print(f'원의 둘레 : {원의_둘레}')
+# print(f'원의 넓이 : {원의_넓이}')
+
+
+#1432. 파이썬 변수 할당
+# apple_korean = '사과'
+# english_korean = '영어'
+# apple_english = 'apple'
+# banana_korean = '바나나'
+# banana_english = 'banana'
+# kiwi_korean = '키위'
+# kiwi_english = 'kiwi'
+
+# print(f'{apple_korean}는 {english_korean}로 {apple_english} \n{banana_korean}는 {english_korean}로 {banana_english} \n{kiwi_korean}는 {english_korean}로 {kiwi_english}')
+
+#1437. 변수를 활용한 연산자
+# 파일명을 python_ws_1_5.py로 작성
+
+# print(3 * 2)
+
+# print(3 ** 2)
+
+# print((3 ** 2) // (3 * 2))
+
+# print((3 ** 2) % (3 * 2))
+
+# print((-3) ** 2)
+
+
+# mutiple = 3 * 2
+# print(mutiple)
+
+# square = 3 ** 2
+# print(square)
+
+# result_share = square // mutiple
+# print(result_share)
+
+# result_remain = square % mutiple
+# print(result_remain)
+
+# negative_square = (-3) ** 2
+# print(negative_square)
+
