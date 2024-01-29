70af7342efbdd9db23e6c4b9e94877bfed45ffcd
Sarah <dhkang960912@gmail.com>
2024-01-30 00:46:50 +0900

    4835 구간합

--- problem_practice/240129_problem.py
+++ problem_practice/240129_problem.py
@@ -23,7 +23,26 @@
 
 # 4835 구간합
 t = int(input())
-for t in range(1, t+1) :
+for t in range(1, t+1):
+    #t번 테스트 케이스를 돌린다.
+    N, M = list(map(int, input().split()))
+    number = list(map(int, input().split()))
+    # N개의 숫자가 주어짐
+    # 구간의 갯수 M이 주어짐
+    Max_N = 0
+    Min_N = 12345e19828
+    for i in range(len(number)) :
+        if Max_N < sum(number[i:M+i]) :
+            Max_N = sum(number[i:M+i])
+
+        if Min_N > sum(number[i:M+i]) :
+            Min_N = sum(number[i:M+i])
+
+    result = Max_N - Min_N
+    print(f'#{t} {result}')
+
+
+
 
 
 
