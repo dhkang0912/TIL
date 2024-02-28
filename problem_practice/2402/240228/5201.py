cd8de127f35d3ae9ad9d2e97b82a2dd94fed3f32
dhkang <dhkang960912@gmail.com>
2024-02-28 15:45:22 +0900

    240228 ssafy study

--- problem_practice/2402/240228/5201.py
+++ problem_practice/2402/240228/5201.py
@@ -7,5 +7,15 @@ T = 1
 for tc in range(1, T+1):
     containers = list(map(int, input().split()))
     trucks = list(map(int, input().split()))
+    N = len(trucks)
     # print(containers)
     # print(trucks)
+    container_weight = containers[0]
+    idx = 0
+    while idx <= N-1:
+        if trucks[idx] >= container_weight:
+            idx += 1
+            containers_weight = containers[idx]
+        else:
+            idx += 1
+
