263af9e9eeafcf787313ea9c22be91cc5e8917ef
dhkang <dhkang960912@gmail.com>
2024-03-20 17:59:38 +0900

    240320 ssafy study

--- problem_practice/2403/240320/contact.py
+++ problem_practice/2403/240320/contact.py
@@ -1,23 +1,25 @@
+# def bfs(s):
+#     Q = []
+#     visited = [False] * 101
+#
+#     Q.append((s, 0))
+#     visited[s] = True
+#
+#     while Q:
+#         v = Q.pop(0)
+#         if maxC < cnt:
+#             maxC = cnt
+#             resV = v
+#         elif maxC == cnt:
+#             resV = max(resV, v)
+#         for w in G[v]:
+#             if not visited[w]:
+#                 Q.append((w, cnt+1))
+#                 visited[w] = True
+
 def bfs(s):
-    Q = []
-    visited = [False] * 101
-
-    Q.append((s, 0))
-    visited[s] = True
-
-    while Q:
-        v = Q.pop(0)
-        if maxC < cnt:
-            maxC = cnt
-            resV = v
-        elif maxC == cnt:
-            resV = max(resV, v)
-        for w in G[v]:
-            if not visited[w]:
-                Q.append((w, cnt+1))
-                visited[w] = True
-
-def bfs(s):
+    global resV
+    maxC = 0
     Q = []
     visited = [False] * 101
 
@@ -25,7 +27,7 @@ def bfs(s):
     visited[s] = 1
 
     while Q:
-        v = Q.pop(0)
+        v, cnt = Q.pop(0)
         if maxC < cnt:
             maxC = cnt
             resV = v
@@ -42,10 +44,11 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     lst = list(map(int, input().split()))
     G = [set() for _ in range(101)]
+    resV = 0
 
     for i in range(0, N, 2):
         v1 = lst[i]
         v2 = lst[i+1]
         G[v1].add(v2)
 
-    bfs(N) # 시작값을 넣어줌
+    bfs(M) # 시작값을 넣어줌
