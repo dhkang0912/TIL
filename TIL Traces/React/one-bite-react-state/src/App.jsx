0e1204c8ec1f2cfd8e0d9432f5959c7153dc3066
dhkang <dkang960912@gmail.com>
2024-07-08 17:31:01 +0900

    one bite react - state

---
+++ TIL Traces/React/one-bite-react-state/src/App.jsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Register from "./Register";
+
+function App() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
+
+export default App;
