f72efe67fbbb5c11878324a1d7ca91185edd51b0
dhkang <dhkang960912@gmail.com>
2024-07-10 17:58:23 +0900

    one bite todolist

---
+++ TIL Traces/React/React One bite/one-bite-section08-todo/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ TIL Traces/React/React One bite/one-bite-section08-todo/src/components/List.jsx
@@ -0,0 +1,12 @@
+import "./List.css"
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List✨</h4>
+      <input placeholder="검색어를 입력하세요" />
+    </div>
+  );
+};
+
+export default List;
