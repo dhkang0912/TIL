380172d782b1dc24989ee922eb2a01546f077e06
dhkang0912 <dhkang960912@gmail.com>
2024-07-11 00:02:32 +0900

    one bite todo create lecture

--- TIL Traces/React/React One bite/one-bite-section08-todo/src/App.jsx
+++ TIL Traces/React/React One bite/one-bite-section08-todo/src/App.jsx
@@ -2,12 +2,56 @@ import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 import "./App.css";
+import { useState, useRef } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    // 현재 날짜를 타임스탬프 형식으로 기록
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    // 현재 날짜를 타임스탬프 형식으로 기록
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    // 현재 날짜를 타임스탬프 형식으로 기록
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  // 임시로 체크하기 위한 데이터
+  const [todos, setTodos] = useState(mockData);
+  // id를 기억하기 위해서는 ref 객체가 필요함
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    // State 값은 직접 바꾸면 되지 않고 무조건 State 함수를 통해 추가해야한다.
+    // 인자에 적힌 배열로 todos를 업데이트함
+    // => newTodo + todos의 내용을 배열에서 펼쳐서 객체들끼리만 모아서 spread syntax를 활용하여 배열에 추가
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- TIL Traces/React/React One bite/one-bite-section08-todo/src/components/Editor.jsx
+++ TIL Traces/React/React One bite/one-bite-section08-todo/src/components/Editor.jsx
@@ -1,9 +1,45 @@
-import "./Editor.css"
-const Editor = ()=>{
-  return <div className="Editor">
-    <input placeholder="새로운 Todo..." />
-    <button>추가</button>
-  </div>
-}
+import "./Editor.css";
+import { useState, useRef } from "react";
 
-export default Editor
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // 키보드의 키값을 누를 때 누른 키의 값이 event에 저장
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      // todo에 추가되도록 함수 호출
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    // content의 내용이 비어있다면 추가되지 않도록 추가 전 강제로 함수 종료하게 return
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    // todo에 추가 이후 content를 초기화하여 입력된 내용 비워주기
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
