68056fa2777cec75b5b9d972a59e0e867b913694
dhkang0912 <dhkang960912@gmail.com>
2024-07-16 00:11:27 +0900

    one bite react 70-71 lecture

--- TIL Traces/React/React One bite/one-bite-section09/src/App.jsx
+++ TIL Traces/React/React One bite/one-bite-section09/src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import Exam from "./components/Exam";
+// import Exam from "./components/Exam";
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 
 const mockData = [
   {
@@ -29,24 +29,44 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    // 새로운 todoItem을 배열에 넣고, 기존 state를 펼쳐서 새로운 배열을 state로 반환함
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
   // 임시로 체크하기 위한 데이터
-  const [todos, setTodos] = useState(mockData);
+  // const [todos, setTodos] = useState(mockData);
+  // 첫번째 인자 = 컴포넌트 외부의 상태관리 함수, 두번째 인자 = 초기값
+  const [todos, dispatch] = useReducer(reducer, mockData);
+
   // id를 기억하기 위해서는 ref 객체가 필요함
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
     // State 값은 직접 바꾸면 되지 않고 무조건 State 함수를 통해 추가해야한다.
     // 인자에 적힌 배열로 todos를 업데이트함
     // => newTodo + todos의 내용을 배열에서 펼쳐서 객체들끼리만 모아서 spread syntax를 활용하여 배열에 추가
-    setTodos([newTodo, ...todos]);
   };
 
   // 조건문으로 길게 작성
@@ -68,27 +88,26 @@ function App() {
   // 삼항연산자 활용하여 간단하게 작성
   const onUpdate = (targetId) => {
     // todos State의 값들 중에 tagetId와 일치하는 id를 갖는 todo Item의 isDone 변경
-    setTodos(
-      todos.map((todo) =>
-        // 만약 targetId와 id가 일치하지 않는 todo라면 isDone을 변경하지 않고 기존 todo를 리턴
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
-  const onDelete = (targetId)=>{
+  const onDelete = (targetId) => {
     // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
     // => targetId와 같지 않은 todo로만 배열을 만들어서 반환
-    setTodos(todos.filter((todo)=>todo.id !== targetId))
-  }
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
 
   return (
     <div className="App">
-      <Exam />
-
-      {/* <Header />
+      <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/> */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
