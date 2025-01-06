bdc4d6ef99b28ceb8e3a7b0fb36a86ec0eacfc49
dhkang <dhkang960912@gmail.com>
2025-01-07 02:16:52 +0900

    250107 코딩 자율학습 자바 입문 3일차 완료

--- TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
+++ TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
@@ -4,22 +4,7 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println(scan.nextLine());
-//        System.out.println(scan.next());
-//        scan.close();
-
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
-        System.out.println(input.nextLine());
-        System.out.println(input.nextLine());
-
-
-
-
-
+    public static void main(String[] args) {
+        System.out.printf("%.4f", 123.45f);
     }
 }
