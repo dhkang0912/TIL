325a897f037960f7609eb026b6369d0865e390e0
dhkang0912 <dhkang960912@gmail.com>
2025-01-08 02:37:37 +0900

    250108 코딩 자율학습 자바 입문 5일차 완료

--- TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
+++ TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
@@ -5,21 +5,15 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("정수를 입력하세요. --> ");
-        int intNum = scan.nextInt();
-
-        System.out.print("실수를 입력하세요. --> ");
-        double doubleNum = scan.nextDouble();
-
-        int add = intNum + (int)doubleNum;
-        double sub = intNum - doubleNum;
-        double mul = intNum * doubleNum;
-        int div = intNum / (int)doubleNum;
-        System.out.println("덧셈 결과(정수): "+ add);
-        System.out.println("뺄셈 결과(실수): "+ sub);
-        System.out.println("곱셈 결과(실수): "+ mul);
-        System.out.println("나눗셈 결과(정수): "+ div);
-
+        int score = 80;
+        if (score >= 90){
+            System.out.println("A학점입니다.");
+        } else if (score < 90 && score>=80 ){
+            System.out.println("B학점입니다.");
+        } else if (score < 80 && score>=70 ){
+            System.out.println("C학점입니다.");
+        } else {
+            System.out.println("D학점입니다.");
+        }
     }
 }
