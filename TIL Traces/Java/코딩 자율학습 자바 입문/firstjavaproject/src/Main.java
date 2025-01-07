c65e830fe700bbe2c1d468b4f215213ed39aaae2
dhkang0912 <dhkang960912@gmail.com>
2025-01-07 13:28:11 +0900

    250107 코딩 자율학습 자바 입문 4일차 완료

--- TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
+++ TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/Main.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("%.4f", 123.45f);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수를 입력하세요. --> ");
+        int intNum = scan.nextInt();
+
+        System.out.print("실수를 입력하세요. --> ");
+        double doubleNum = scan.nextDouble();
+
+        int add = intNum + (int)doubleNum;
+        double sub = intNum - doubleNum;
+        double mul = intNum * doubleNum;
+        int div = intNum / (int)doubleNum;
+        System.out.println("덧셈 결과(정수): "+ add);
+        System.out.println("뺄셈 결과(실수): "+ sub);
+        System.out.println("곱셈 결과(실수): "+ mul);
+        System.out.println("나눗셈 결과(정수): "+ div);
+
     }
 }
