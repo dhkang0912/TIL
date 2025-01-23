d40beb401f937d6c336babf9371298a3751a68a5
dhkang0912 <dhkang960912@gmail.com>
2025-01-23 17:09:00 +0900

    250123 코딩 자율학습 자바 16일차 완료

---
+++ TIL Traces/Java/코딩 자율학습 자바 입문/firstjavaproject/src/ch10/Boxing.java
@@ -0,0 +1,21 @@
+package ch10;
+
+public class Main {
+    public static void main(String[] args){
+        int a = 5;
+        // 명시적 박싱
+        Integer aObj = new Integer(a);
+
+        // 오토 박싱
+        int b = 10;
+        Integer bObj = b;
+
+        // 명시적 언박싱
+        Integer numObj = new Integer(10);
+        int num = numObj.intValue();
+
+        // 오토 언박싱
+        Integer numObj2 = 10;
+        int num2 = numObj2;
+    }
+}
