116b29f3740bf164a06a30e577a452744e9bc157
dhkang0912 <dhkang960912@gmail.com>
2026-05-21 00:26:31 +0900

    260521 코딩자율학습 스프링부트 4일차

--- TIL Traces/Java/demo/src/main/java/com/example/demo/dto/ArticleForm.java
+++ TIL Traces/Java/demo/src/main/java/com/example/demo/dto/ArticleForm.java
@@ -1,24 +1,15 @@
 package com.example.demo.dto;
 
 import com.example.demo.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;
     private String content;
     private Long id;
-
-    public ArticleForm(String content, String title) {
-        this.content = content;
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
