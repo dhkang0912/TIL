691ce9dc4e5c472ff2585bfdf33aa72e864aa58e
kangdoohong <dh2kang@infiniq.co.kr>
2026-05-27 21:17:50 +0900

    코딩자율학습 스프링부트 5일차

--- TIL Traces/Java/demo/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ TIL Traces/Java/demo/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.repository.CrudRepository;
 import com.example.demo.entity.Article;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll(); // Iterable → ArrayList 수정
 }
